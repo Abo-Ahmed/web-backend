c17415251cb38e1dc2f4a55d0b6c15c9b4dfd4fc
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2021-07-24 14:48:59 +0200

    done

---
+++ projects/HellowordServerlet/WebserverUniversity/src/Download.java
@@ -0,0 +1,38 @@
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Download
+ */
+@WebServlet("/Download")
+public class Download extends HttpServlet {
+
+	public Download ()
+	{
+		super();	
+		
+	}
+	
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		String n=request.getParameter("name"); 
+		System.out.println(n);  
+	}
+
+}
---
+++ projects/HellowordServerlet/projectW/src/HelloWorld.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+		System.out.println("constuctor  Register");
+	    
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter printWriter  = response.getWriter();
+		printWriter.println("<h3>Hello World Hello World </h3>");
+		System.out.println("constuctor  Get");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("constuctor  Post");
+
+	}
+
+}
